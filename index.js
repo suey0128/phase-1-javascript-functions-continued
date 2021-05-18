d7f5147f2558ca67085cae8bf8eee49b9966c7fa
suey0128 <syui0128@gmail.com>
2021-05-17 19:13:10 -0700

    commit tree 8b57eb8c2a9f59ddd31940402f451f27edb0c853

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+// function saturdayFun (fun){
+//     if (typeof fun === 'undefined') {
+//         return `This Saturday, I want to roller-skate!`
+//     }
+//     else {
+//         return `This Saturday, I want to ${fun}!`
+//     }
+// }
+
+function saturdayFun (fun = "roller-skate"){ 
+    return `This Saturday, I want to ${fun}!`;
+}
+
+// function mondayWork (work){
+//     if (typeof work === 'undefined') {
+//         return "This Monday, I will go to the office."
+//     }
+//     else {
+//         return `This Monday, I will ${work}.`
+//     }
+// }
+
+function mondayWork (work = "go to the office"){ 
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective (y="*") {
+    return function (x = "special") {
+        return `You are ${y}${x}${y}!`;
+    }
+}
